158db196017d27e32620ad2149b802ecd6a9e490
Dan Shultz <das0118@gmail.com>
2016-06-22 17:31:50 -0400

    ensure all migrations are run a tables are created

--- test/db/migrate_test.js
+++ test/db/migrate_test.js
@@ -2,18 +2,39 @@ const { expect } = require('chai');
 const db = require('../../lib/db');
 
 const latestMigration = '1';
+const createdTables = ['migrations', 'accounts', 'transactions'];
 
 describe('migrating', function () {
+  afterEach(function () {
+    db.clearInstances();
+  });
+
   it('migrates a new database', function (done) {
     let instance = db.getInstance(':memory:');
 
     db.migrate(instance)
-      .then(() => instance.get('SELECT migration from migrations order by migration DESC limit 1'))
+      .then(() => instance.get('SELECT migration from migrations order by migration DESC'))
       .then((result) => {
         expect(result).to.deep.equal({ migration: latestMigration });
         done();
       })
       .catch(done);
+  });
+
+  it('creates all the tables', function (done) {
+    let instance = db.getInstance(':memory:');
+
+    db.migrate(instance)
+      .then(() => instance.all('SELECT name FROM sqlite_master WHERE type=\'table\''))
+      .then((result) => {
+        let tableNames = result.map((r) => r.name);
+        createdTables.forEach((table) => {
+          expect(tableNames).to.include(table);
+        });
+        done();
+      })
+      .catch(done);
+
   });
 
 });
