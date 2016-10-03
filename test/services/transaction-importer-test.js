e123217579350845736f8aa817749af1b863bd5e
Dan Shultz <das0118@gmail.com>
2016-10-02 21:42:33 -0400

    create transaction importer

--- test/services/transaction-importer-test.js
+++ test/services/transaction-importer-test.js
@@ -3,17 +3,19 @@ const db = require('../../lib/db');
 const TransactionImporter = require('../../lib/services/transaction-importer');
 
 var transactions = [{
-	amount: -2300,
-	fitId: '219867',
-	name: 'Interest Charge',
-	postedAt: '2005-08-11 08:00:00',
-	type: 'INT'
+  amount: -2300,
+  fitId: '219867',
+  name: 'Interest Charge',
+  postedAt: '2005-08-11 08:00:00',
+  type: 'INT',
+  balance: 40
 }, {
-	amount: 35000,
-	fitId: '219868',
-	name: 'Payment - Thank You',
-	postedAt: '2005-08-11 08:00:00',
-	type: 'CREDIT'
+  amount: 35000,
+  fitId: '219868',
+  name: 'Payment - Thank You',
+  postedAt: '2005-08-11 08:00:00',
+  type: 'CREDIT',
+  balance: 40
 }];
 
 
@@ -37,17 +39,17 @@ describe('importing transactions', function () {
       .then((data) => {
         expect(data.length).to.equal(2);
         expect(data[0]).to.deep.contain({
-					fit_id: '219867',
-					type: 'INT',
-					date: '2005-08-11 08:00:00',
-					description: null,
-					memo: null,
-					name: 'Interest Charge',
-					payee: null,
-					check_number: null,
-					amount: -2300,
-					balance: 40,
-					account_id: 4
+          fit_id: '219867',
+          type: 'INT',
+          date: '2005-08-11 08:00:00',
+          description: null,
+          memo: null,
+          name: 'Interest Charge',
+          payee: null,
+          check_number: null,
+          amount: -2300,
+          balance: 40,
+          account_id: 4
         });
         done();
       })
