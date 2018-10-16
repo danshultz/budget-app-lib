22f1b6f8e0f5abb8a92f155b7f20dd8826a1ca6f
Dan Shultz <das0118@gmail.com>
2018-10-15 22:25:48 -0400

    Enable setting categories on transactions

---
+++ test/services/category-set-service-test.js
@@ -0,0 +1,75 @@
+const chai = require('chai');
+const { expect, assert } = chai;
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+const { noopLogger } = require('../support');
+const db = require('../../lib/db');
+const dbUtils = require('../../lib/db/actions/util');
+const CategorySetService = require('../../lib/services/category-set-service');
+
+var transactionData = {
+  account_id: 1,
+  amount: -1250,
+  balance: 9000,
+  check_number: '',
+  date: '2018-10-11 12:00:00',
+  description: null,
+  fit_id: '4223317601201810111',
+  memo: 'WALMART DEBIT CRD ACH TRAN',
+  name: 'ACH DEBIT XXXXX1122',
+  payee: '',
+  type: 'DEBIT',
+}
+
+describe('setting categories', function () {
+  beforeEach(function (done) {
+    let instance = this.instance = db.getInstance(':memory:');
+    db.migrate(instance, noopLogger).then(done).catch(done);
+  });
+
+  afterEach(function () {
+    db.clearInstances();
+  });
+
+  it('fails when attempting to set categories for an invalid transaction', function () {
+    let categorySetService = new CategorySetService(this.instance);
+    return expect(categorySetService.setCategories(99, null)).to.be.rejectedWith("transaction with id 99 doesn't exist");
+  })
+
+  it('sets categories', function () {
+    let categorySetService = new CategorySetService(this.instance);
+    let categories = [
+      { key: 'office_supplies', amount: -650 },
+      { key: 'gifts', amount: -600 },
+    ];
+
+    // create record to update
+    return dbUtils.insert(this.instance, 'transactions', transactionData)
+      .then((transactionRecord) => {
+        // set the categories
+        return categorySetService.setCategories(transactionRecord.id, categories)
+      })
+      // verify the categories were updated correctly
+      .then((updatedRecord) => {
+        expect(updatedRecord).to.deep.contain({
+          id: 1,
+          categories: categories
+        });
+      })
+  })
+
+  it('it throws an error for categories that do not total the amount', function () {
+    let categorySetService = new CategorySetService(this.instance);
+    let categories = [{ key: 'office_supplies', amount: -650 }];
+
+    // create record to update
+    return dbUtils.insert(this.instance, 'transactions', transactionData)
+      .then((transactionRecord) => {
+        // attempt to set the categories
+        return expect(categorySetService.setCategories(transactionRecord.id, categories))
+          .to.be.rejectedWith('categories sum -650 must add up to transaction amount of -1250');
+      })
+  })
+
+})
