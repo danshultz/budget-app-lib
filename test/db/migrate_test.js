aad2944bcb3ca16796527e46aa84b790a1218dd3
Dan Shultz <das0118@gmail.com>
2018-12-04 09:44:08 -0500

    Add budget tables

--- test/db/migrate_test.js
+++ test/db/migrate_test.js
@@ -2,54 +2,51 @@ const { expect } = require('chai');
 const { noopLogger } = require('../support');
 const db = require('../../lib/db');
 
-const latestMigration = '5';
-const createdTables = ['migrations', 'accounts', 'transactions'];
+const latestMigration = '6';
+const createdTables = [
+  'accounts',
+  'budgets',
+  'budget_entries', 'migrations',
+  'transactions',
+];
 
 describe('migrating', function () {
   afterEach(function () {
     db.clearInstances();
   });
 
-  it('migrates a new database', function (done) {
+  it('migrates a new database', function () {
     let instance = db.getInstance(':memory:');
 
-    db.migrate(instance, noopLogger)
+    return db.migrate(instance, noopLogger)
       .then(() => instance.get('SELECT migration from migrations order by migration DESC'))
       .then((result) => {
         expect(result).to.deep.equal({ migration: latestMigration });
-        done();
       })
-      .catch(done);
   });
 
-  it('creates all the tables', function (done) {
+  it('creates all the tables', function () {
     let instance = db.getInstance(':memory:');
 
-    db.migrate(instance, noopLogger)
+    return db.migrate(instance, noopLogger)
       .then(() => instance.all('SELECT name FROM sqlite_master WHERE type=\'table\''))
       .then((result) => {
         let tableNames = result.map((r) => r.name);
         createdTables.forEach((table) => {
           expect(tableNames).to.include(table);
         });
-        done();
       })
-      .catch(done);
-
   });
 
-  it('adds the categories JSON column properly', function (done) {
+  it('adds the categories JSON column properly', function () {
     let instance = db.getInstance(':memory:');
 
-    db.migrate(instance, noopLogger)
+    return db.migrate(instance, noopLogger)
       .then(() => instance.all('PRAGMA table_info(transactions);'))
       .then((result) => {
         const column = result.find((d) => d.name === 'categories')
         expect(column).to.include({ type: 'JSON1', notnull: 1, dflt_value: "'[]'" })
-        done();
       })
-      .catch(done);
-
   });
 
 });
